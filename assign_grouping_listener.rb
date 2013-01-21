857914cf811367ff8892446675b9481917878fea
akira-kuriyama <marronshepherd2@gmail.com>
2013-01-22 03:19:26 +0900

    first commit

---
+++ assign_grouping_listener.rb
@@ -0,0 +1,61 @@
+class AssignGroupingListener < Redmine::Hook::ViewListener
+  def view_issues_edit_notes_bottom(context)
+
+	issue = context[:issue]
+	assignable_users = issue.assignable_users
+	group_ids = (assignable_users.map {|user| user.groups.map {|group| group.id}}).flatten.uniq
+	assignable_groups = Group.find(group_ids)
+
+	groups = [] 
+	assignable_groups.each do |group| 
+		users = []
+		assignable_users.each do |user| 
+			user.groups.each do |ugroup|
+				if ugroup.id == group.id
+					users << {'name' => user.name, 'id' => user.id}
+				end
+			end
+		end
+		groups << {'lastname' => group.lastname, 'users' => users }
+	end
+	assignable_users.each do |user| 
+		is_belong = false
+		groups.each do |group| 
+			group['users'].each do |guser|
+				if guser['id'] == user.id 
+					is_belong = true
+				end
+			end
+		end
+		if !is_belong
+			independent_group = groups.find {|g| g['lastname'] == '----'} 
+			if !independent_group 
+				independent_group = {'lastname' => '----', 'users' => []}
+				groups.unshift(independent_group)
+			end
+			independent_group['users'] << {'name' => user.name, 'id' => user.id}
+		end
+		if User.current.id == user.id 
+			independent_group = groups.find {|g| g['lastname'] == '----'} 
+			if !independent_group 
+				independent_group = {'lastname' => '----', 'users' => []}
+				groups.unshift(independent_group)
+			end
+			independent_group['users'].unshift({'name' => "<< #{l(:label_me)} >>", 'id' => user.id})
+
+		end
+	end
+	
+	#Rails.logger.info(groups)
+
+	context[:controller].send(
+       		:render_to_string,
+		{
+		  :partial => 'hooks/assign_grouping',
+		  :layout  => false, 
+		  :locals => { :groups => groups }
+		}
+      )
+  end
+end
+
---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+require 'assign_grouping_listener'
+Redmine::Plugin.register :redmine_assign_grouping do
+  name 'Redmine Assign Grouping plugin'
+  author 'marronshepherd2@gmail.com'
+  description 'grouping issue assinged'
+  version '0.0.1'
+  url 'https://github.com/akira-kuriyama/redmine_assign_grouping'
+end
+
